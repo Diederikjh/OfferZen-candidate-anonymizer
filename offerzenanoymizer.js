6753f0217afd749c079dcec74fe6045e828a8f55
Diederik Hattingh <djh@emss.co.za>
2018-02-09 12:21:30 +0200

    Small cleanup. Added TODO list

--- offerzenanoymizer.js
+++ offerzenanoymizer.js
@@ -1,32 +1,37 @@
-  // Profile images
 
-  var profileImages = document.getElementsByClassName("profile-image")
-  Array.prototype.forEach.call(profileImages, function(element) {
+/*
 
-  	// Image
-  	element.setAttribute("src", "http://thecatapi.com/api/images/get?format=src");
-  	//element.removeAttribute("src");
+	TODO:
+		Initial load sometiems still shows profile picture.
+		Show enabled/disabled on UI
+		Would be nice to have same random cat picture for list view as details view
 
+
+*/
+
+var fixPrifileImagesListView = function (nodes) {
+	Array.prototype.forEach.call(nodes, function(element) {
+	  	element.setAttribute("src", "http://thecatapi.com/api/images/get?format=src");
+	  });
+}
+
+var fixEducationListView = function (nodes){
+  var replacementUniversityText = "University of Somewhere"
+  Array.prototype.forEach.call(nodes, function(element) {
+  	console.log(element.childNodes.length)
+	for (var i = 0; i < element.childNodes.length; i++) {
+		if (element.childNodes[i].nodeName === "#text") {
+			if (element.childNodes[i].nodeValue) {
+				element.childNodes[i].nodeValue = element.childNodes[i].nodeValue.replace(/.*University.*/, replacementUniversityText)
+				element.childNodes[i].nodeValue = element.childNodes[i].nodeValue.replace(/Unisa/, replacementUniversityText)
+			}
+		}
+	}
   });
+}
 
-  // Main page profile image
-  var avatarDivs = document.getElementsByClassName("avatar")
-  Array.prototype.forEach.call(avatarDivs, function(element) {
-
-  	// Image
-  	element.setAttribute("style", "background-image:url('http://thecatapi.com/api/images/get?format=src')");
-
-  });
-
-  // Name on list view
-
-  var cardHeaders = document.getElementsByClassName("card-header")
-  Array.prototype.forEach.call(cardHeaders, function(element) {
-  	// TODO only replace name, not entire diff
-  	// Name 
-  	//element.innerText = "A human just like you"
-
-  	// https://stackoverflow.com/a/6520270/8524
+var fixNameListView = function (nodes) {
+	Array.prototype.forEach.call(nodes, function(element) {
   	for (var i = 0; i < element.childNodes.length; i++) {
 	    var curNode = element.childNodes[i];
 		    if (curNode.nodeName === "#text") {
@@ -37,12 +42,28 @@
 		}
 
   });
+}
 
+  var profileImages = document.getElementsByClassName("profile-image")
+  fixPrifileImagesListView(profileImages);
+
+  // Hide university names
+  var education = document.getElementsByClassName("education")
+  fixEducationListView(education)
+  
+  // Name on list view
+  var cardHeaders = document.getElementsByClassName("card-header")
+  fixNameListView(cardHeaders)
+
+  // Main page profile image
+  var avatarDivs = document.getElementsByClassName("avatar")
+  Array.prototype.forEach.call(avatarDivs, function(element) {
+  	element.setAttribute("style", "background-image:url('http://thecatapi.com/api/images/get?format=src')");
+  });
 
   // Name on details view
   var cardHeaders = document.getElementsByClassName("card-content")
   Array.prototype.forEach.call(cardHeaders, function(element) {
-  	// Name 
   	console.log(element.childNodes.length)
 	for (var i = 0; i < element.childNodes.length; i++) {
 		if (element.childNodes[i].nodeName === "H3") {
@@ -56,22 +77,33 @@
   document.title.replace(/.* - /, "")
 
 
-var replacementUniversityText = "University of Somewhere"
-// Hide university names
-var education = document.getElementsByClassName("education")
-  Array.prototype.forEach.call(education, function(element) {
-  	// Name 
-  	console.log(element.childNodes.length)
-	for (var i = 0; i < element.childNodes.length; i++) {
-		if (element.childNodes[i].nodeName === "#text") {
-			if (element.childNodes[i].nodeValue) {
-				element.childNodes[i].nodeValue = element.childNodes[i].nodeValue.replace(/.*University.*/, replacementUniversityText)
-				element.childNodes[i].nodeValue = element.childNodes[i].nodeValue.replace(/Unisa/, replacementUniversityText)
+// Watch for changes here to candidate list node:
+var config = { childList: true };
+var candidateList = document.getElementsByClassName("candidate-list")
+
+var callback = function(mutationsList) {
+    for(var mutation of mutationsList) {
+        if (mutation.type == 'childList') {
+			for(var addedNodes of mutation.addedNodes) {
+
+				// Do listview stuff
+            	var profileImages = addedNodes.getElementsByClassName("profile-image")
+            	fixPrifileImagesListView(profileImages)
+
+   			    var education = addedNodes.getElementsByClassName("education")
+  			    fixEducationListView(education)
+  			    
+    			var cardHeaders = addedNodes.getElementsByClassName("card-header")
+  			    fixNameListView(cardHeaders)
+
 			}
-		}
-	}
-  });
 
-//  TODO replace elements added dynamically on listview
+        }
+    }
+};
 
+// Create an observer instance linked to the callback function
+var observer = new MutationObserver(callback);
 
+// Start observing the target node for configured mutations
+observer.observe(candidateList[0], config);
